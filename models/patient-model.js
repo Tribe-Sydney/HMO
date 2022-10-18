2601053acfeff8097942e9be8836f8136a53c8f9
Joshua Olutunde <joshuafavour81@gmail.com>
2022-10-18 07:58:38 -0700

    added patient model

--- models/patient-model.js
+++ models/patient-model.js
@@ -2,22 +2,57 @@ const { default: mongoose } = require("mongoose");
 const User = require("../utils/user");
 
 const patient = {
-    role: {
+    location: {
       type: String,
-      enum: ["approved", "not-approved"],
-      default: "not-approved",
+      required: true
     },
-    available: {
-      type: Boolean,
-    },
-    setAvailableTime: {
+    address: {
       type: String,
+      required: true
     },
-    ratings: {
-      type: Number,
-      default: null,
+    occupation: {
+      type: String,
+      required:true
     },
-    certification: {
+    description: {
       type: String,
     },
   };
+
+  const patientSchema = new mongoose.Schema(
+    {...User, ...patient},
+    { toObject: { virtuals: true }, toJSON: { virtuals: true } }
+  )
+
+  patientSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next()
+
+    let salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    this.passwordConfirm = undefined
+    next()
+  })
+
+  patientSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString("hex");
+  
+    this.passwordResetToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+    this.passwordTokenExpires = Date.now() + 10 * 60 * 1000;
+    return resetToken;
+  };
+  
+  patientSchema.methods.changePasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
+      console.log(JWTTimestamp < this.passwordChangedAt);
+      return JWTTimestamp < this.passwordChangedAt;
+    }
+    return false;
+  };
+  
+  const Patient = mongoose.model("Patient", patientSchema);
+  
+  module.exports = Patient;
+  
